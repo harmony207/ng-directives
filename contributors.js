0e1d07ed03e9d29af27ce2abf16932c571ea528c
Harmony Proxy <harmony.developer@yahoo.com>
2015-09-04 15:38:16 +0200

    Create contributors.js

---
+++ contributors.js
@@ -0,0 +1,12 @@
+[
+    {
+        name: "Harmony Mothibe",
+        position: "Full Stack Developer",
+        country: "South Africa",
+        links: {
+            googleplus: "https://plus.google.com/+HarmonyDevelopermothivhe/posts",
+            twitter: '',
+            facebook: '',
+        }
+    }
+]
